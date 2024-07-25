0e8655fcd31c089123dd13332824ba65f9965624
zhaojs <zhaopromise@gmail.com>
2024-07-25 10:59:38 +0800

    fix:logo

---
+++ docs/assets/onMountedOrActivated-6KdOBnfv.js
@@ -0,0 +1,1 @@
+import{o as e,y as n,P as a}from"./vue-oIE3a0W0.js";function d(t){let o;e(()=>{t(),n(()=>{o=!0})}),a(()=>{o&&t()})}export{d as o};
--- src/layouts/default/footer/index.vue
+++ src/layouts/default/footer/index.vue
@@ -7,7 +7,7 @@
 
       <a @click="openWindow(DOC_URL)">{{ t('layout.footer.onlineDocument') }}</a>
     </div>
-    <div>Copyright &copy;2020 Vben Admin</div>
+    <div>Copyright &copy;2024 UILIST</div>
   </Layout.Footer>
 </template>
 <script lang="ts" setup>
