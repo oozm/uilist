e27ed0836c4d5048a4c1024ba3c8f8179abf90e3
zhaojs <zhaopromise@gmail.com>
2024-07-31 10:39:02 +0800

    fix：deploy

--- docs/assets/onMountedOrActivated-6KdOBnfv.js
+++
@@ -1,1 +0,0 @@
-import{o as e,y as n,P as a}from"./vue-oIE3a0W0.js";function d(t){let o;e(()=>{t(),n(()=>{o=!0})}),a(()=>{o&&t()})}export{d as o};
--- docs/assets/siteSetting-BUuynIQ3.js
+++
@@ -1,1 +0,0 @@
-const t="https://github.com/oozm/uilist",s="https://github.com/oozm/uilist",o="https://uilist.com/";export{s as D,t as G,o as S};
